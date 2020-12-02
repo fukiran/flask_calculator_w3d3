7338965f381537ef4b2831192793c3fad9b498ac
fukiran <macdorphoto@yahoo.com>
2020-12-02 21:26:28 +0000

    The answer is

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,15 +3,15 @@ from modules import calculator
 
 @app.route('/add/<num_1>/<num_2>')
 def add(num_1,num_2):
-    return str(calculator.add(int(num_1),int(num_2)))
+    return "The answer is  " + str(calculator.add(int(num_1),int(num_2)))
 
 @app.route('/subtract/<num_1>/<num_2>')
 def subtract(num_1,num_2):
-    return str(calculator.subtract(int(num_1),int(num_2)))
+    return "The answer is  " + str(calculator.subtract(int(num_1),int(num_2)))
 
 @app.route('/multiply/<num_1>/<num_2>')
 def multiply(num_1,num_2):
-    return str(calculator.multiply(int(num_1),int(num_2)))
+    return "The answer is  " + str(calculator.multiply(int(num_1),int(num_2)))
 
 @app.route('/divide/<num_1>/<num_2>')
 def divide(num_1,num_2):
